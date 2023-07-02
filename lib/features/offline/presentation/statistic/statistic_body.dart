899eea7a2b0b0d02adafd7ae02750722bf58cbac
Tai Phan Van <taipv.swe@gmail.com>
2023-07-02 09:25:01 +0700

    test bar chart

--- lib/features/offline/presentation/statistic/statistic_body.dart
+++ lib/features/offline/presentation/statistic/statistic_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_note/core/common/view_status.dart';
 import 'package:game_note/features/offline/presentation/statistic/bloc/statistic_bloc.dart';
+import 'package:game_note/features/offline/presentation/statistic/widgets/total_statistic.dart';
 
 class StatisticBody extends StatefulWidget {
   const StatisticBody({Key? key}) : super(key: key);
@@ -27,21 +28,11 @@ class _StatisticBodyState extends State<StatisticBody> {
         );
       }
       if (state.viewStatus.isSuccess) {
-        return ListView.builder(
-          itemCount: state.listStatistic.length,
-          itemBuilder: (context, index) => Column(
-            children: [
-              Text(state.listStatistic[index].playerModel.fullname),
-              Text('Tỉ lệ thắng ${state.listStatistic[index].percentWin}'),
-              Text('Tỉ lệ hòa ${state.listStatistic[index].percentDraw}'),
-              Text('Tỉ lệ thua ${state.listStatistic[index].percentLose}'),
-              Text(
-                  'Điểm trung bình mỗi trận ${state.listStatistic[index].pointPerMatch}'),
-              Text(
-                  '${state.listStatistic[index].totalWinsDrawLose} / ${state.listStatistic[index].countMatches}'),
-            ],
-          ),
+        // poit permatch
+        state.listStatistic.sort(
+          (a, b) => b.pointPerMatch.compareTo(a.pointPerMatch),
         );
+        return TotalStatistic(statistics: state.listStatistic);
       }
       return const SizedBox.shrink();
     });
---
+++ lib/features/offline/presentation/statistic/widgets/total_statistic.dart
@@ -0,0 +1,109 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+import 'package:game_note/features/offline/presentation/statistic/models/personal_statistic.dart';
+
+class TotalStatistic extends StatelessWidget {
+  final List<PersonalStatistic> statistics;
+  const TotalStatistic({
+    Key? key,
+    required this.statistics,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // calcualate
+    return BarChart(BarChartData(
+      barTouchData: barTouchData,
+      titlesData: titlesData,
+      borderData: borderData,
+      barGroups: barGroups,
+      gridData: const FlGridData(show: false),
+      alignment: BarChartAlignment.spaceAround,
+      maxY: 3,
+    ));
+  }
+
+  LinearGradient get _barsGradient => const LinearGradient(
+        colors: [
+          Colors.deepOrange,
+          Colors.orange,
+        ],
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+      );
+
+  FlBorderData get borderData => FlBorderData(
+        show: false,
+      );
+
+  BarTouchData get barTouchData => BarTouchData(
+        enabled: false,
+        touchTooltipData: BarTouchTooltipData(
+          tooltipBgColor: Colors.transparent,
+          tooltipPadding: EdgeInsets.zero,
+          tooltipMargin: 8,
+          getTooltipItem: (
+            BarChartGroupData group,
+            int groupIndex,
+            BarChartRodData rod,
+            int rodIndex,
+          ) {
+            return BarTooltipItem(
+              rod.toY.toStringAsFixed(2),
+              const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      );
+  Widget getTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text = statistics[value.toInt()].playerModel.fullname;
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+        show: true,
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            getTitlesWidget: getTitles,
+          ),
+        ),
+        leftTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      );
+
+  List<BarChartGroupData> get barGroups => statistics
+      .mapIndexed((index, e) => BarChartGroupData(
+            x: index,
+            barRods: [
+              BarChartRodData(
+                toY: e.pointPerMatch,
+                gradient: _barsGradient,
+              )
+            ],
+            showingTooltipIndicators: [0],
+          ))
+      .toList();
+}
